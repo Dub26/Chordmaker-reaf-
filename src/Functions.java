014af69e1ac2afb397f8961e5400e8547f2ee4a6
Dub26 <153666042+Dub26@users.noreply.github.com>
2024-01-15 07:59:59 +0300

    I finished the scales
    I updated my scale writer. I separated my scales to major and minor and it worked. My next step will writing a basic major and minor chord function.

--- src/Functions.java
+++ src/Functions.java
@@ -1,38 +1,70 @@
 public class Functions {
-    public Note[] major(String s,int length){
-        int[] w={2,2,1,2,2,2,1};
+    public Note[] pattern(String s,int length,String x){
+        int[] w={};
+        int[] wa={2,2,1,2,2,2,1};
+        int[] wb={2,1,2,2,1,2,2};
+        if(x.trim().equalsIgnoreCase("major"))  w=wa;
+        if(x.trim().equalsIgnoreCase("minor"))  w=wb;
         Notes notes=new Notes();
         Note[] con=new Note[length];
+
         for(int i=0;i<length;i++){
             con[i]=new Note();
         }
-        int dt=0;
-        for(int i=0;i<12;i++){
-            if(s.equalsIgnoreCase(notes.notes[i].name)) break;
-            dt++;
-        }
-        con[0].name=notes.notes[dt].name;
+
+
+        int dt=whichnote(s);
+
+        con[0].name=s;
         int a=0;
         for(int i=0;i<length-1;i++){
-            a+=w[i];
+            a+=w[i%7];
             if(notes.notes[(dt+a)%12].name.equalsIgnoreCase("N")){
-                con[i+1].name=deterw(notes.notes[dt].name,(dt+a)%12);
+                if(x.trim().equalsIgnoreCase("minor")) {
+                    s=notes.notes[(dt+3)%12].name;
+                }
+                con[i+1].name=flatoraunt(s,(dt+a)%12);
                 continue;
             }
             con[i+1].name=notes.notes[(dt+a)%12].name;
         }
         return con;
     }
-    public String deterw(String f,int whn){
+    public String flatoraunt(String f,int whn){
         Notes notes=new Notes();
-        String[] aunt={"g","d","a","e","b"};
-        String[] flat={"f"};
-        if(f.equalsIgnoreCase(flat[0])) return notes.notes[whn+1].name+"b";
+        String[] aunt={"g","d","a","e","b","F#","C#"};
+        String[] flat={"f","bb","eb","ab","db","gb","cb"};
+        for(int i=0;i< flat.length;i++){
+            if(f.equalsIgnoreCase(flat[i])) return notes.notes[whn+1].name+"b";
+        }
 
         for(int i=0;i<aunt.length;i++) {
             if (f.equalsIgnoreCase(aunt[i])) return notes.notes[whn - 1].name + "#";
         }
         return "a";
+    }
+    public int whichnote(String s){
+        Notes notes=new Notes();
+        String af="#b";
+        int dt=0;
+        if(s.length()>1) {
+            if(s.charAt(1)==af.charAt(0)) dt++;
+            else dt--;
+            String g=s.substring(0,1);
+            for(int i=0;i<12;i++){
+                if(g.equalsIgnoreCase(notes.notes[i].name)){
+                    break;
+                }
+                dt++;
+            }
+
+        } else{
+            for(int i=0;i<12;i++){
+                if(s.equalsIgnoreCase(notes.notes[i].name)) break;
+                dt++;
+            }
+        }
+        return dt;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Functions f=new Functions();
-        Note[] gamC=new Note[7];
-        gamC=f.major("A", gamC.length);
+        Note[] gamC=new Note[20];
+        gamC=f.pattern("B", gamC.length,"minor");
         for(int i=0;i< gamC.length;i++){
             System.out.println(gamC[i].name);
         }
